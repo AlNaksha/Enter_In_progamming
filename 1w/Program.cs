6f2cd362020338b4f16cca6825776dac6301fe5a
Farnakeev Alex <Lexity@yandex.ru>
2023-09-24 22:45:18 +0300

    First commit

---
+++ 1w/Program.cs
@@ -0,0 +1,25 @@
+﻿//Console.WriteLine("213123Hello, World!");
+Console.WriteLine("Введите номер задачи: ");
+int ntask = Convert.ToInt32(Console.ReadLine());
+switch(ntask){
+    case 1:{
+        Console.WriteLine("1");
+        break;
+    };
+    case 2:{
+        Console.WriteLine("2");
+        break;
+    };
+    case 3:{
+        Console.WriteLine("3");
+        break;
+    };
+    case 4:{
+        Console.WriteLine("4");
+        break;
+    };
+    default:{
+        Console.WriteLine("else");
+        break;
+    };
+}
