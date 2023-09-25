beda224c77b80b75c12f4fe9e6ca0fc71e8ebb8b
Farnakeev Alex <Lexity@yandex.ru>
2023-09-25 19:33:19 +0300

    add task 2 and 4

--- 1w/Program.cs
+++ 1w/Program.cs
@@ -1,25 +1,61 @@
 ﻿//Console.WriteLine("213123Hello, World!");
-Console.WriteLine("Введите номер задачи: ");
+
+void Task2(){
+    Console.Write("\nПервое число:");
+    int NumA = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Второе число:");
+    int NumB = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+    if (NumA>NumB) {
+        Console.Write("Максимальное значение: {0}\nМинимальное: {1}", NumA, NumB);
+    }
+    else{
+        Console.Write("Минимальное значение: {0}\nМинимальное: {1}", NumB, NumA);
+    }
+}
+
+void Task4(){
+    Console.Write("\nВведите последовательно 3 числа: ");
+
+    int[] Mass = Console.ReadLine().Split(' ').Select(x=>int.Parse(x)).ToArray(); // да, подглядел
+    Console.Write("Максимальное число: {0}", Mass.Max());
+
+    // int i=0;
+    // int[] Mass = new int[3];
+    // while(i<3){
+    //     Mass[i] = Convert.ToInt32(Console.ReadLine());
+    //     i++;
+    // }
+    // Console.Write("{0} {1} {2}", Mass[0], Mass[1], Mass[2]);
+
+}
+
+Console.Write(@"Доступные номера задач:
+    2 Максимальное из двух целых чисел.
+    4 Максимальное из трех целых чисел.
+    6 Проверка на четность.
+    8 Вывод всех четных до вводимого числа.
+Введите номер задачи: ");
 int ntask = Convert.ToInt32(Console.ReadLine());
 switch(ntask){
-    case 1:{
-        Console.WriteLine("1");
-        break;
-    };
     case 2:{
-        Console.WriteLine("2");
-        break;
-    };
-    case 3:{
-        Console.WriteLine("3");
+        Task2();
         break;
     };
     case 4:{
+        Task4();
+        break;
+    };
+    case 6:{
+        Console.WriteLine("3");
+        break;
+    };
+    case 8:{
         Console.WriteLine("4");
         break;
     };
     default:{
-        Console.WriteLine("else");
+        Console.WriteLine("Попробуй еще раз.");
         break;
     };
 }
