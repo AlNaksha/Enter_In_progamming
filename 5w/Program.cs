f03db078e98646201c22adde85ee75a775fe156c
Farnakeev Alex <Lexity@yandex.ru>
2023-10-06 21:07:26 +0300

    6w 40 41 44 45

---
+++ 5w/Program.cs
@@ -0,0 +1,144 @@
+﻿/*
+// Задача 31: Задайте массив из N элементов, заполненный случайными числами из промежутка [-9, 9]. 
+// Найдите сумму отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
+
+void FillArray(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(-9, 10);
+    }
+}
+
+System.Console.Write("Введите кол-во элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[size];
+int sumPositive = 0;
+int sumNegative = 0;
+
+FillArray(arr);
+System.Console.WriteLine(string.Join(", ", arr));
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        sumPositive += arr[i];
+    }
+    else
+    {
+        sumNegative += arr[i];
+    }
+}
+System.Console.WriteLine($"Сумма положительных = {sumPositive}, а сумма отрицательных = {sumNegative}");
+
+//Вариант №2
+void FillArray(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(-9, 10);
+    }
+}
+
+void SumPositiveAndNegative(int[] arr, out int sumPositive, out int sumNegative)
+{
+    sumPositive = 0;
+    sumNegative = 0;
+    
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            sumPositive += arr[i];
+        }
+        else
+        {
+            sumNegative += arr[i];
+        }
+    }
+}
+
+System.Console.Write("Введите кол-во элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[size];
+
+
+FillArray(arr);
+System.Console.WriteLine(string.Join(", ", arr));
+SumPositiveAndNegative(arr, out int sumP, out int sumN);
+System.Console.WriteLine($"Сумма положительных = {sumP}, а сумма отрицательных = {sumN}");
+/**/
+// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+
+void FillArray(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 1000);
+    }
+}
+
+Console.Write("Введите кол-во элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[size];
+
+FillArray(arr);
+Console.WriteLine(string.Join(", ", arr));
+int counter = 0;
+
+for (int  i = 0; i < arr.Length; i++ )
+{
+    if (arr[i] >= 10 && arr[i] <= 99)
+    {
+        counter++;
+    }
+}
+
+Console.WriteLine(counter);
+
+/*
+void FillArray(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(1, 10);
+    }
+}
+
+Console.Write("Введите кол-во элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[size];
+
+FillArray(arr);
+Console.WriteLine(string.Join(", ", arr));
+
+int[] result;
+
+if (size % 2 == 0)
+{
+    result = new int[size / 2];
+} else
+{
+    result = new int[size / 2 + 1];
+    result[size / 2] = arr[size / 2];
+}
+
+for (int i = 0; i < arr.Length / 2 ; i++)
+{
+    result[i] = arr[i] * arr[arr.Length - 1 - i];
+}
+
+Console.WriteLine(string.Join(", ", result));
+/**/
---
+++ 6w/Program.cs
@@ -0,0 +1,159 @@
+﻿/*// Задача 40:
+Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник со сторонами такой длины.
+Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+///
+
+int getInt(string str){ // Получение целого числа с консоли с текстом запроса
+    Console.Write(str);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int a = getInt("Введите a: ");
+int b = getInt("Введите b: ");
+int c = getInt("Введите c: ");
+
+if((a+b>c)&&(a+c>b)&&(c+b>a)){
+    Console.Write("Треуг сущ.");
+}
+else{
+    Console.Write("Треуг не сущ.");
+}
+*/
+
+/*// Задача 45:
+Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+///
+int getInt(string str){ // Получение целого числа с консоли с текстом запроса
+
+    Console.Write(str);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] getArrRnd(int n, int s, int e){ // Генерация массива заданного размера со значениями заданного диапазона
+
+    int[] mass = new int[n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < n; i++)
+    {
+        mass[i] = rnd.Next(s, e);
+    }
+    return mass;
+}
+
+int[] copyArr(int[] arrbase){
+
+    int[] arr2 = new int[arrbase.Length];
+
+    for (int i = 0; i<arrbase.Length; i++){
+        arr2[i] = arrbase[i];
+    }
+    return arr2;
+}
+
+
+int[] arr = getArrRnd(getInt("Введите число элементов: "), 0, 100);
+
+int[] arr2 = copyArr(arr);
+
+Console.WriteLine(string.Join(", ", arr));
+Console.WriteLine(string.Join(", ", arr2));
+// */
+
+/*// Задача 44:
+Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8
+Если введено отрицательное число то задается ряд в обратном порядке
+///
+
+int getInt(string str){ // Получение целого числа с консоли с текстом запроса
+
+    Console.Write(str);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] getFiboArr(int n){
+    if (n == 0){
+        return;
+
+    } else {
+        int lngth = Math.Abs(n);
+        int[] fibo = new int[lngth];
+        
+        switch(lngth){
+            default:
+                
+                int istt = n>0?2:lngth-1;
+                int iend = n>0?lngth-1:2;
+
+            case 2: fibo[n>0?1:0]=1;
+            case 1: fibo[n>0?0:1]=0;
+                if (lngth>2){
+                    for (int i = 2; i < lngth; i++){
+                        fibo[i-lngth-1]=fibo[i-1+lngth]+fibo[i-2+lngth];
+                    }                     
+                }
+        }
+    }
+
+    return fibo;
+}
+
+int[] Fibo = getFiboArr(getInt("Введите длинну ряда Фибоначи"));
+
+Console.WriteLine(string.Join(", ", Fibo));
+
+//*/
+
+/*// Задача 41:
+Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+
+///
+
+int[] getArrConsole(){
+    Console.Write($"\nВведите элементы массива через пробел или запятую: ");
+    string[] str = Console.ReadLine().Split(' ',',');
+    int[] arr = new int[str.Length];
+    for(int i=0; i<str.Length; i++) arr[i] = int.Parse(str[i]);
+    return arr; 
+}
+
+int countPos(int[] arr){
+    int count=0;
+    for(int i=0; i<arr.Length; i++){
+        if(arr[i]>0) count++;
+    }
+    return count;
+}
+
+int[] arr = getArrConsole();
+Console.Write(string.Join(", ", arr)+" -> "+countPos(arr));
+
+//*/
+
+/*//Задача 43:
+Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+*///
+int getInt(string str){ // Получение целого числа с консоли с текстом запроса
+
+    Console.Write(str);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] intrsec(int[] line2d1, int[] line2d2){
+    int x = (line2d1[2]-line2d2[2])/(line2d2[1]-line2d1[1]);
+    int y = line2d1[1]*x+line2d1[2];
+    return {x, y};
+}
+
+Console.WriteLine(string.Join(", ", intrsec({getInt("k1 = "),getInt("b1 = ")}, {getInt("k2 = "),getInt("b2 = ")})));
+
+
+//*/
